17f8076c506a697f2ec5a14bad3dbc4bd94392eb
Abhay <77850911+Abhay056@users.noreply.github.com>
2024-05-23 17:29:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
-import telegram 
-from telegram.ext import Updater, CommandHandler 
-import requests 
-import datetime
+import telegram
+from telegram.ext import Updater, CommandHandler
+import requests
 import random
- 
-def start(update, context): 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm a news bot. Send me /news to get the latest headlines.") 
- 
-def news(update, context): 
-    # Send a request to the Codeforces API to get the problem 
-    api_key = "006bd70a81b066df6f1003d594ed5e6cc4c9abe7"     
-    secret = "96e2964abe55f0bb755f18f9d0fa00da2b843040"
-    tags = input()
-    time = datetime.now()
-    rand = random.randint(100000,999999)
-    url = f"https://codeforces.com/api/problemset.problems?tags={tags}&apiKey={api_key}&time={time}&apiSig={rand}sha512Hex({rand}/problemset.problems?tags={tags}&apiKey={api_key}&time={time}#{secret})" 
-    response = requests.get(url) 
 
-    # Construct a message to get question from codeforces
-    message = response.json()["Problem"]
- 
-    # Send the message to the user 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message) 
- 
-# Set up the Updater and Dispatcher 
-updater = Updater(token="", use_context=True) 
-dispatcher = updater.dispatcher 
- 
+TOKEN = '6961128694:AAEIo_uOkefBc5ihGnWvPmocmLLY21p9Vgw'
+bot = telegram.Bot(token=TOKEN)
 
+def start(update, context):
+    update.message.reply_text("Welcome to Codeforces Random Question Generator Bot! Use /random <tag(s)> <rating> to get a random problem.")
 
+def random_problem(update, context):
+    try:
+        tags = context.args[:-1]
+        rating = int(context.args[-1])
 
+        url = f"https://codeforces.com/api/problemset.problems?tags={';'.join(tags)}&minRating={rating}&maxRating={rating}"
+        response = requests.get(url)
+        data = response.json()
 
+        if data["status"] == "OK":
+            problems = data["result"]["problems"]
+            if problems:
+                random_problem = random.choice(problems)
+                problem_name = random_problem["name"]
+                problem_url = f"https://codeforces.com/problemset/problem/{random_problem['contestId']}/{random_problem['index']}"
+                update.message.reply_text(f"Random problem: {problem_name}\nLink: {problem_url}")
+            else:
+                update.message.reply_text("No problems found with the given parameters.")
+        else:
+            update.message.reply_text("Failed to fetch problems. Please try again later.")
+    except (IndexError, ValueError):
+        update.message.reply_text("Usage: /give <tag(s)> <rating>")
 
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("give", random_problem))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
