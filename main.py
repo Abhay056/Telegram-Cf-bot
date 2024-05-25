d28d779fc6393405a5de9be00ebb59fd4705ba72
Abhay <77850911+Abhay056@users.noreply.github.com>
2024-05-25 14:48:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, request
 import telegram
 from telegram.ext import Application, CommandHandler, ContextTypes, Updater
 from telegram import Update
@@ -7,11 +8,19 @@ import asyncio
 import os
 
 TOKEN = '6961128694:AAEO_nZQwVlFg6fGmfT1GsS0NoH-7qImADo'
+app = Flask(__name__)
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        Application.process_update(update)
+    return 'ok'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Welcome to Codeforces Random Question Bot! \n"
-        "For help type /help.\n Type /give <your_handle> <rating> , and optionally specify a <tag> to filter questions.\n"
+        "For help type /help.\n Type /give <your_handle> <rating> and optionally specify a <tag> to filter questions.\n"
     )
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -20,13 +29,13 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Commands:\n"
         "/start - Starts the bot\n"
         "/help - List of commands and how to use them\n"
-        "/give <your_handle> <rating> - Get a random question. Replace <your_handle> with your Codeforces username, <rating> with your preferred rating, and optionally specify a tag to filter questions.\n"
+        "/give <your_handle> <rating> - Get a random question. Replace <your_handle> with your Codeforces username, <rating> with your preferred rating, and optionally specify a <tag> to filter questions.\n"
     )
     
 async def get_random_question(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if len(context.args) < 2:
         await update.message.reply_text(
-            "Please provide your Codeforces handle and the preferred rating (e.g., /give username 1700 [tag])."
+            "Please provide your Codeforces handle and the preferred rating (e.g., /give username 1700 tag[optional])."
         )
         return
 
@@ -44,12 +53,24 @@ async def get_random_question(update: Update, context: ContextTypes.DEFAULT_TYPE
         user_data = user_response.json()
         if user_data['status'] != "OK":
             raise Exception(f"Error: {user_data['comment']}")
+        try:
+            rating = int(rating)
+        except ValueError:
+            await update.message.reply_text("Invalid rating. Please provide a numerical value.")
+            return
 
+        min_rating = 800 
+        max_rating = 3600  
+        
+        if rating < min_rating or rating > max_rating:
+            await update.message.reply_text(f"No problems found, please enter rating within the range {min_rating} - {max_rating}.")
+            return 
+        
         problems_response = requests.get("https://codeforces.com/api/problemset.problems")
         problems_data = problems_response.json()
         if problems_data['status'] != "OK":
             raise Exception("Failed to fetch problems from Codeforces.")
-
+        
         problems = problems_data['result']['problems']
         filtered_problems = [
             problem for problem in problems 
@@ -77,6 +98,7 @@ def main():
     bot.add_handler(CommandHandler("start", start))
     bot.add_handler(CommandHandler("help", help_command))
     bot.add_handler(CommandHandler("give", get_random_question))
+
     bot.run_polling()
 
 if __name__ == '__main__':
